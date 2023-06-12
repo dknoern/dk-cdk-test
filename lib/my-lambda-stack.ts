547f4c0c769c8f88bc328583ee92d8cbbab19c02
David Knoernschild <david@seattleweb.com>
2023-06-12 16:18:53 -0700

    add bucket

--- lib/my-lambda-stack.ts
+++ lib/my-lambda-stack.ts
@@ -1,10 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 import { Function, InlineCode, Runtime } from 'aws-cdk-lib/aws-lambda';
 
 export class MyLambdaStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props);
+
+      // create bucket 
+      const bucket = new s3.Bucket(this, 'MyFirstBucket', { 
+        versioned: true,
+        encryption: s3.BucketEncryption.KMS_MANAGED,
+        blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+        enforceSSL: true,
+        autoDeleteObjects: true,
+        removalPolicy: cdk.RemovalPolicy.DESTROY
+        }
+        );
+                          
+          
 
       new Function(this, 'LambdaFunction', {
         runtime: Runtime.NODEJS_18_X,
